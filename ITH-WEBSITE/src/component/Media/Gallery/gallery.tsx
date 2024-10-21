6ea46b0976447a16585ca2608d9678981e8eb181
taiolamz <hassanadefarati@gmail.com>
2024-10-21 16:13:56 +0100

    quick update

--- ITH-WEBSITE/src/component/Media/Gallery/gallery.tsx
+++ ITH-WEBSITE/src/component/Media/Gallery/gallery.tsx
@@ -5,9 +5,18 @@ const Gallery = () => {
   return (
     <Section className="h-screen flex flex-col justify-center">
       <p>Gallery</p>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {gallery.map((chi, idx) => (
-          <img key={idx} src={chi} alt="gallery-image" />
+          <div
+            key={idx}
+            className={`relative ${idx === 0 ? "row-span-2" : ""}`}
+          >
+            <img
+              src={chi}
+              alt={`gallery-image-${idx}`}
+              className="w-full h-auto object-cover rounded-lg"
+            />
+          </div>
         ))}
       </div>
     </Section>
--- ITH-WEBSITE/src/component/Media/Gallery/lists.ts
+++ ITH-WEBSITE/src/component/Media/Gallery/lists.ts
@@ -10,4 +10,5 @@ export const gallery = [
   GalleryImageTwo,
   GalleryImageThree,
   GalleryImageFour,
+  GalleryImageFour,
 ];
